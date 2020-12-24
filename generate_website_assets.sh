ae0cb80a25923b4b25d342a0fd00f898299073dc
Denis Merigoux <denis.merigoux@gmail.com>
2020-12-24 16:09:40 +0100

    Various fixes for French tutorial

--- generate_website_assets.sh
+++ generate_website_assets.sh
@@ -12,7 +12,8 @@ make website-assets
 rsync -a _build/default/_doc/_html/ $1/ocaml_docs/
 scp examples/allocations_familiales/allocations_familiales.html $1/
 scp examples/us_tax_code/us_tax_code.html $1/
-scp examples/tutorial/tutorial_en.html $1/
+scp examples/tutorial_en/tutorial_en.html $1/
+scp examples/tutoriel_fr/tutoriel_fr.html $1/
 scp grammar.html $1/
 scp catala.html $1/
 scp _build/default/src/catala.bc.js $1/playground/
