2b5a54e315accd47bac6952c08d772d36e3ffca6
Bartosz Woźniak <bartoszwoz0@gmail.com>
2021-05-16 20:13:03 +0200

    atom stuff

---
+++ syntax_highlighting/pl/setup_atom.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env bash
+
+FILE=~/.atom/packages/catala_pl
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+
+if [ ! -L "$FILE" ]; then
+  echo "Creating link"
+  ln -s -f $SCRIPTPATH/atom "$FILE"
+fi
