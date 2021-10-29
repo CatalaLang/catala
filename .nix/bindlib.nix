cfcc45402220a024897cb599c55ebaac1b812a11
Alain <alain@inria.fr>
2021-10-29 14:45:05 +0200

    feat: initial nix support

---
+++ .nix/bindlib.nix
@@ -0,0 +1,30 @@
+{ lib
+, fetchFromGitHub
+, buildDunePackage
+}:
+
+# We need the very last version "bleeding edge" since previous versions don't use dune.
+
+buildDunePackage rec {
+  pname = "bindlib";
+  version = "5.0.1a";
+
+  minimumOCamlVersion = "4.0.8";
+
+  useDune2 = true;
+
+  src = fetchFromGitHub {
+    owner = "rlepigre";
+    repo = "ocaml-${pname}";
+    rev = "317f195d22c75f556053039cd94b52bd0c423709";
+    name = pname;
+    hash = "sha256-uO/Ko9PmQ+wE0d9jfEngd4G014B4nxGgfQyEvB52Pz8=";
+  };
+
+  meta = with lib; {
+    homepage = "https://rlepigre.github.io/ocaml-bindlib/";
+    description = "Bindlib is a library allowing the manipulation of data structures with bound variables";
+    license = licenses.lgpl3;
+    maintainers = [ ];
+  };
+}
---
+++ default.nix
@@ -0,0 +1,64 @@
+{ lib
+, fetchFromGitHub
+, buildDunePackage
+, ansiterminal
+, sedlex_2
+, menhir
+, unionfind
+, bindlib
+, cmdliner
+, re
+, zarith
+, zarith_stubs_js
+, ocamlgraph
+, calendar
+, visitors
+, benchmark
+, js_of_ocaml
+, js_of_ocaml-ppx
+, camomile
+, cppo
+}:
+
+buildDunePackage rec {
+  pname = "catala";
+  version = "0.3.0";
+
+  minimumOCamlVersion = "4.08";
+
+  src = ./.;
+
+  useDune2 = true;
+
+  propagatedBuildInputs = [
+    ansiterminal
+    sedlex_2
+    menhir
+    cmdliner
+    re
+    zarith
+    zarith_stubs_js
+    ocamlgraph
+    calendar
+    visitors
+    benchmark
+    js_of_ocaml
+    js_of_ocaml-ppx
+    camomile
+    cppo
+
+    unionfind
+    bindlib
+  ];
+  doCheck = true;
+
+  patches = [ ./.nix/no-web.patch ];
+
+
+  meta = with lib; {
+    homepage = "https://catala-lang.org";
+    description = "Catala is a domain-specific programming language designed for deriving correct-by-construction implementations from legislative texts.";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ ];
+  };
+}
