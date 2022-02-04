424b68a6a5b3df9ef717e2b5f5b8763031bc576f
Alain <alain@inria.fr>
2022-02-04 16:43:59 +0100

    formatting

--- compiler/lcalc/optimizations.ml
+++ compiler/lcalc/optimizations.ml
@@ -69,15 +69,13 @@ let rec iota_expr (_ : unit) (e : expr Pos.marked) : expr Pos.marked Bindlib.box
       and+ case = visitor_map iota_expr () (List.nth cases i) in
       default_mark @@ EApp (case, [ e1 ])
   | EMatch (e', cases, n)
-    when begin
-           cases
-           |> List.mapi (fun i (case, _pos) ->
-                  match case with
-                  | EInj (_ei, i', n', _ts') ->
-                      i = i' && (* n = n' *) Dcalc.Ast.EnumName.compare n n' = 0
-                  | _ -> false)
-           |> List.for_all Fun.id
-         end ->
+    when cases
+         |> List.mapi (fun i (case, _pos) ->
+                match case with
+                | EInj (_ei, i', n', _ts') ->
+                    i = i' && (* n = n' *) Dcalc.Ast.EnumName.compare n n' = 0
+                | _ -> false)
+         |> List.for_all Fun.id ->
       visitor_map iota_expr () e'
   | _ -> visitor_map iota_expr () e
 
@@ -85,8 +83,7 @@ let rec beta_expr (_ : unit) (e : expr Pos.marked) : expr Pos.marked Bindlib.box
   let default_mark e' = Pos.same_pos_as e' e in
   match Pos.unmark e with
   | EApp (e1, args) -> (
-      let+ e1 = beta_expr () e1
-      and+ args = List.map (beta_expr ()) args |> Bindlib.box_list in
+      let+ e1 = beta_expr () e1 and+ args = List.map (beta_expr ()) args |> Bindlib.box_list in
       match Pos.unmark e1 with
       | EAbs ((binder, _pos_binder), _ts) ->
           let (_ : (_, _) Bindlib.mbinder) = binder in
@@ -105,9 +102,7 @@ let rec peephole_expr (_ : unit) (e : expr Pos.marked) : expr Pos.marked Bindlib
 
   match Pos.unmark e with
   | EIfThenElse (e1, e2, e3) -> (
-      let+ e1 = peephole_expr () e1
-      and+ e2 = peephole_expr () e2
-      and+ e3 = peephole_expr () e3 in
+      let+ e1 = peephole_expr () e1 and+ e2 = peephole_expr () e2 and+ e3 = peephole_expr () e3 in
       match Pos.unmark e1 with
       | ELit (LBool true) | EApp ((EOp (Unop (Log _)), _), [ (ELit (LBool true), _) ]) -> e2
       | ELit (LBool false) | EApp ((EOp (Unop (Log _)), _), [ (ELit (LBool false), _) ]) -> e3
@@ -118,4 +113,4 @@ let peephole_optimizations (p : program) : program =
   { p with scopes = List.map (fun (var, e) -> (var, Bindlib.unbox (peephole_expr () e))) p.scopes }
 
 let optimize_program (p : program) : program =
-  p |> iota_optimizations |> peephole_optimizations |> beta_optimizations |> beta_optimizations |> beta_optimizations |> beta_optimizations |> beta_optimizations |> beta_optimizations |> beta_optimizations |> beta_optimizations |> beta_optimizations |> beta_optimizations |> beta_optimizations |> beta_optimizations
+  p |> iota_optimizations |> peephole_optimizations |> beta_optimizations
