0a9a593c61cdfcfff19327648a6da3f34ca1a53d
Denis Merigoux <denis.merigoux@gmail.com>
2022-09-26 19:23:30 +0200

    Restore base input

--- french_law/python/cnaf_cross_tester/main.py
+++ french_law/python/cnaf_cross_tester/main.py
@@ -5,7 +5,18 @@ from pupeteer import run_simulator
 from input import AppartementOuMaison, AppartementOuMaisonType, CnafSimulatorInput, Enfant, SeulOuCouple, Zone
 from random_input_generator import generate_random_input
 
-# Output identical to the JS test of the housing benefits
+# input identical to the JS test of the housing benefits
+sample_input = CnafSimulatorInput(
+    zone=Zone.Zone2,
+    logement=AppartementOuMaison(
+        AppartementOuMaisonType.Location, meuble=False),
+    loyer=450,
+    seul_ou_couple=SeulOuCouple.EnCouple,
+    enfants=[Enfant(age=7, remuneration_derniere_annee=0),
+             Enfant(age=8, remuneration_derniere_annee=0)],
+    revenu_pris_en_compte=11_500
+)
+# Or a random input
 sample_input = generate_random_input()
 print("🏡 Description du ménage")
 print(sample_input)
