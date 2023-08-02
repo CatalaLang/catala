3b1c23c054f9c39d6a30740493c401073498ea3f
Micah Cantor <micahcantor01@gmail.com>
2023-08-02 12:17:28 +0200

    add explicit yojson open

--- runtimes/ocaml/runtime.ml
+++ runtimes/ocaml/runtime.ml
@@ -13,6 +13,7 @@
    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
    License for the specific language governing permissions and limitations under
    the License. *)
+open Ppx_yojson_conv_lib.Yojson_conv.Primitives
 
 type nonrec unit = unit
 type nonrec bool = bool
