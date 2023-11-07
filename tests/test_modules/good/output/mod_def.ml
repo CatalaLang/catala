d00cac09adc3b9bb368aba60a8b684c56d27630f
Louis Gesbert <louis.gesbert@ocamlpro.com>
2023-11-07 11:52:09 +0100

    Harmless test results changes

--- tests/test_modules/good/output/mod_def.ml
+++ tests/test_modules/good/output/mod_def.ml
@@ -31,9 +31,19 @@ let s (s_in: S_in.t) : S.t =
     try
       (handle_default
          {filename = ""; start_line=0; start_column=1;
-           end_line=0; end_column=1; law_headings=[]} ([||])
-         (fun (_: unit) -> true)
-         (fun (_: unit) -> money_of_cents_string "100000"))
+           end_line=0; end_column=1; law_headings=[]}
+         ([|(fun (_: unit) ->
+               handle_default
+                 {filename = ""; start_line=0; start_column=1;
+                   end_line=0; end_column=1; law_headings=[]} ([||])
+                 (fun (_: unit) -> true)
+                 (fun (_: unit) -> money_of_cents_string "100000"))|])
+         (fun (_: unit) -> false)
+         (fun (_: unit) -> try (raise EmptyError) with
+            EmptyError -> (raise (NoValueProvided
+              {filename = "tests/test_modules/good/mod_def.catala_en";
+                start_line=16; start_column=10; end_line=16; end_column=12;
+                law_headings=["Test modules + inclusions 1"]}))))
     with
     EmptyError -> (raise (NoValueProvided
       {filename = "tests/test_modules/good/mod_def.catala_en"; start_line=16;
@@ -43,8 +53,18 @@ let s (s_in: S_in.t) : S.t =
     try
       (handle_default
          {filename = ""; start_line=0; start_column=1;
-           end_line=0; end_column=1; law_headings=[]} ([||])
-         (fun (_: unit) -> true) (fun (_: unit) -> Enum1.Maybe ()))
+           end_line=0; end_column=1; law_headings=[]}
+         ([|(fun (_: unit) ->
+               handle_default
+                 {filename = ""; start_line=0; start_column=1;
+                   end_line=0; end_column=1; law_headings=[]} ([||])
+                 (fun (_: unit) -> true) (fun (_: unit) -> Enum1.Maybe ()))|])
+         (fun (_: unit) -> false)
+         (fun (_: unit) -> try (raise EmptyError) with
+            EmptyError -> (raise (NoValueProvided
+              {filename = "tests/test_modules/good/mod_def.catala_en";
+                start_line=17; start_column=10; end_line=17; end_column=12;
+                law_headings=["Test modules + inclusions 1"]}))))
     with
     EmptyError -> (raise (NoValueProvided
       {filename = "tests/test_modules/good/mod_def.catala_en"; start_line=17;
