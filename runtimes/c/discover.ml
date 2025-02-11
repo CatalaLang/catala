a4e5cfa8c520ca5514b4292d5bae932fff83264e
vbot <vincent.botbol@inria.fr>
2025-02-11 18:16:17 +0100

    Fix C runtime build on windows

---
+++ runtimes/c/discover.ml
@@ -0,0 +1,22 @@
+let () =
+  let open Re in
+  if Array.length Sys.argv <> 2 then
+    failwith "expected either ar or gmp_flags as argument"
+  else
+    match Sys.argv.(1) with
+    | "ar" ->
+      (* On mingw's opam windows install gcc is named: x86_64-w64-mingw32-gcc,
+         and, ar is named: x86_64-w64-mingw32-ar. We substitute gcc by ar from
+         the c compiler retrieved in the OCaml compiler's config to make the
+         build system handle it properly. *)
+      let r = compile (seq [str "gcc"; opt (str ".exe"); eol]) in
+      if (Sys.win32 || Sys.cygwin) && Re.execp r Config.c_compiler then
+        Config.c_compiler |> replace_string r ~by:"ar" |> print_string
+      else print_string "ar"
+    | "gmp_flags" ->
+      if Sys.win32 || Sys.cygwin then
+        (* On cygwin/windows, pkg-config is not aware of gmp but opam is aware
+           of the include dir. *)
+        ()
+      else exit @@ Sys.command "pkg-config --cflags gmp"
+    | s -> Format.ksprintf failwith "unknown command: %s" s
