c69b185c055e4e98f1632d7629008c44fbc7771c
Louis Gesbert <louis.gesbert@inria.fr>
2025-03-28 17:44:38 +0100

    Fix duplication of some attrs, update tests

--- compiler/shared_ast/expr.mli
+++ compiler/shared_ast/expr.mli
@@ -193,6 +193,9 @@ val attrs : ('a, 'm) gexpr -> (attr_path * attr_value Mark.pos) list
 val set_attrs :
   ('a, 'm) gexpr -> (attr_path * attr_value Mark.pos) list -> ('a, 'm) gexpr
 
+val no_attrs : 'm mark -> 'm mark
+(** Discards the attributes: useful when copying a mark around. *)
+
 val no_mark : 'm mark -> 'm mark
 (** Returns an empty mark, using the argument as type witness. Note that the
     custom part is kept on [Custom] marks *)
