fa10f81a173bcee657d1332cb3bb820fd6cecb84
PRIMET Romain <romain.primet@inria.fr>
2025-03-31 13:41:01 +0200

    add maven project skeleton

---
+++ runtimes/java/src/test/java/org/catala_lang/catala/RuntimeTest.java
@@ -0,0 +1,19 @@
+package org.catala_lang.catala;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class RuntimeTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
