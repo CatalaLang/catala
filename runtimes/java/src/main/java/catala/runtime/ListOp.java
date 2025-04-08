225069958408d22bbd276fceeeacd604d338e7b3
PRIMET Romain <romain.primet@inria.fr>
2025-04-08 14:28:50 +0200

    reduce

--- runtimes/java/src/main/java/catala/runtime/ListOp.java
+++ runtimes/java/src/main/java/catala/runtime/ListOp.java
@@ -1,17 +1,25 @@
 package catala.runtime;
 
-import java.util.function.Predicate;
+import java.util.function.BiFunction;
 import java.util.stream.Stream;
 
 
-public class ListOp {
+public final class ListOp {
     public static <T extends CatalaValue, R extends CatalaValue> R[] map(CatalaFunction<T,R> func, T[] arr){
         return Stream.of(arr).map(func).toArray(size -> (R[]) new CatalaValue[size]);
     }
 
-    public static <T extends CatalaValue> filter(CatalaPredicate<T> func, T[] arr){
-        return Stream.of(arr).filter();
+    public static <T extends CatalaValue> T[] filter(CatalaFunction<T, CatalaBool> func, T[] arr){
+        return Stream.of(arr).filter(CatalaPredicate.ofCatalaFunction(func)).toArray(size -> (T[]) new CatalaValue[size]);
+    }
+    
+    //map2
+
+    //reduce
+    public static <T extends CatalaValue> T reduce(BiFunction<T, T, T> reducer, CatalaFunction<CatalaUnit, T> dflt, T[] arr){
+        return Stream.of(arr).reduce((a, b) -> reducer.apply(a, b)).orElse(dflt.apply(CatalaUnit.INSTANCE));
     }
 
+    //fold_left
 
 }
