219ac05c90eab9d7fdd3bb369c75c46dea6c1abd
Louis Gesbert <louis.gesbert@inria.fr>
2025-05-16 12:07:42 +0200

    C test mains: duplicate defs in main file (not working)

--- tests/modules/good/prorata_external.c
+++ tests/modules/good/prorata_external.c
@@ -2,34 +2,24 @@
 #include <stdlib.h>
 #include <catala_runtime.h>
 
-/* const catala_code_position pos = {__FILE__, 0, 0, 0, 0}; */
-
-/* const CATALA_ARRAY(CATALA_MONEY) ProrataExternal__prorata
- *     (CATALA_MONEY amount, const CATALA_ARRAY(CATALA_MONEY) weights)
- * {
- *   int i;
- *   mpz_t remaining;
- *   CATALA_INT count = catala_new_int(weights->size);
- *   CATALA_ARRAY(CATALA_MONEY) const prorata =
- *     catala_malloc(sizeof(catala_array));
- *   mpz_init_set(remaining, amount);
- *   prorata->size = weights->size;
- *   prorata->elements = catala_malloc(prorata->size * sizeof(void*));
- *   for (i = 0; i < prorata->size - 1; i++) {
- *     CATALA_MONEY m = o_div_mon_int(&pos, amount, count);
- *     prorata->elements[i] = m;
- *     mpz_sub(remaining, remaining, m);
- *   }
- *   return prorata;
- * } */
+const catala_code_position pos = {__FILE__, 0, 0, 0, 0};
 
 const CATALA_ARRAY(CATALA_MONEY) ProrataExternal__prorata
     (CATALA_MONEY amount, const CATALA_ARRAY(CATALA_MONEY) weights)
 {
+  int i;
+  mpz_t remaining;
+  CATALA_INT count = catala_new_int(weights->size);
   CATALA_ARRAY(CATALA_MONEY) const prorata =
     catala_malloc(sizeof(catala_array));
-  prorata->size = 0;
-  printf("PRO1\n");
+  mpz_init_set(remaining, amount);
+  prorata->size = weights->size;
+  prorata->elements = catala_malloc(prorata->size * sizeof(void*));
+  for (i = 0; i < prorata->size - 1; i++) {
+    CATALA_MONEY m = o_div_mon_int(&pos, amount, count);
+    prorata->elements[i] = m;
+    mpz_sub(remaining, remaining, m);
+  }
   return prorata;
 }
 
