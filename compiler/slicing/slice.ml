e030236bc0fd683e160542e3db4d6952ce6a04ef
Samuel <samuel.avril@ens-lyon.fr>
2025-05-21 14:54:35 +0200

    Correct type of Trace_ast.t

--- compiler/slicing/slice.ml
+++ compiler/slicing/slice.ml
@@ -1,3 +1,3 @@
 open Shared_ast
 
-let slice_trace (trace_with_holes : 'm Trace_ast.t) : 'm program = assert false
+let slice_trace (trace_with_holes : ('a, 'm) Trace_ast.t) : 'a program = assert false
