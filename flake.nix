81777abf03e67d25c8626ec55ccb9318200b5945
Florian RICHER <florian.richer@protonmail.com>
2025-09-16 14:15:20 +0200

    flake: Update and simplify the configuration

--- flake.nix
+++ flake.nix
@@ -1,21 +1,17 @@
 {
   inputs = {
-    opam-repository = {
-      url = "github:ocaml/opam-repository";
-      flake = false;
-    };
     opam-nix.url = "github:tweag/opam-nix";
-    opam-nix.inputs.opam-repository.follows = "opam-repository";
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.follows = "opam-nix/nixpkgs";
   };
-  outputs = { self, flake-utils, opam-nix, nixpkgs, ... }@inputs:
+  outputs = { flake-utils, opam-nix, nixpkgs, ... }:
+    let
+      package = "catala";
+    in 
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         on = opam-nix.lib.${system};
-        localPackagesQuery = builtins.mapAttrs (_: pkgs.lib.last)
-          (on.listRepo (on.makeOpamRepo ./.));
         devPackagesQuery = {
           # You can add "development" packages here. They will get added to the devShell automatically.
           ocaml-lsp-server = "*";
@@ -28,7 +24,7 @@
         query = devPackagesQuery // {
           ## You can force versions of certain packages here, e.g:
           ## - force the ocaml compiler to be taken from opam-repository:
-          # ocaml-base-compiler = "*";
+          ocaml-base-compiler = "*";
           ## - or force the compiler to be taken from nixpkgs and be a certain version:
           # ocaml-system = "4.14.0";
           ## - or force ocamlfind to be a certain version:
@@ -42,23 +38,21 @@
             });
             # You can add overrides here
           };
-        scope' = scope.overrideScope' overlay;
+        scope' = scope.overrideScope overlay;
+        # The main package containing the executable
+        main = scope'.${package};
         # Packages from devPackagesQuery
-        devPackages = builtins.attrValues
-          (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope');
-        # Packages in this workspace
-        packages =
-          pkgs.lib.getAttrs (builtins.attrNames localPackagesQuery) scope';
+        devPackages = builtins.attrValues (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope');
       in {
         legacyPackages = scope';
 
-        #inherit packages;
-
-        ## If you want to have a "default" package which will be built with just `nix build`, do this instead of `inherit packages;`:
-        packages = packages // { default = packages.catala; };
+        packages = {
+          default = main;
+          ${package} = main;
+        };
 
         devShells.default = pkgs.mkShell {
-          inputsFrom = builtins.attrValues packages;
+          inputsFrom = [ main ];
           buildInputs = devPackages ++ [
             # You can add packages from nixpkgs here
             pkgs.inotify-tools
