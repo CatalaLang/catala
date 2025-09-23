bb0646188c43a22a6ebf2d354ff9f1a964a49f57
Vincent Botbol <vincent.botbol@inria.fr>
2025-09-23 15:31:19 +0200

    Output absolute file paths in Json reports

--- build_system/clerk_report.ml
+++ build_system/clerk_report.ml
@@ -551,7 +551,8 @@ let print_json ~(build_dir : string) (tests : file list) =
   let pos_to_json ((s, e) : Lexing.position * Lexing.position) : Yojson.t =
     `Assoc
       [
-        "fname", `String (File.remove_prefix build_dir s.pos_fname);
+        ( "fname",
+          `String File.(Sys.getcwd () / remove_prefix build_dir s.pos_fname) );
         "start_lnum", `Int s.pos_lnum;
         "start_cnum", `Int s.pos_cnum;
         "end_lnum", `Int e.pos_lnum;
@@ -565,7 +566,9 @@ let print_json ~(build_dir : string) (tests : file list) =
            Some
              (`Assoc
                [
-                 "file", `String (File.remove_prefix build_dir test.name);
+                 ( "file",
+                   `String
+                     File.(Sys.getcwd () / remove_prefix build_dir test.name) );
                  ( "tests",
                    `Assoc
                      [
