aed9ef52baed5f8aef059a1fb154788f20afa09c
octachron <octa@polychoron.fr>
2025-09-24 11:16:39 +0200

    Avoid duplicating File.Map

--- compiler/catala_utils/pos_map.mli
+++ compiler/catala_utils/pos_map.mli
@@ -2,12 +2,11 @@ type coverage = Positive | Negative | Fulfilled
 type small_pos = { line : int; col : int }
 type loc_interval = { start : small_pos; stop : small_pos }
 
-module Filemap : Stdlib.Map.S with type key = string
-
 type t
 
 val empty : t
-val export : t -> (loc_interval * coverage) list Filemap.t
+val export : t -> (loc_interval * coverage) list File.Map.t
+
 val add : Pos.t -> coverage -> t -> t
 val fusion : t -> t -> t
 val report_coverage : Format.formatter -> t -> unit
