ec68c79e85976ccc0ce0504df39efba43ab6e160
Louis Gesbert <louis.gesbert@inria.fr>
2025-10-15 14:49:45 +0200

    Stdlib: Add tests for Period externals

--- stdlib/ocaml/period_internal.ml
+++ stdlib/ocaml/period_internal.ml
@@ -7,7 +7,7 @@ let cmp = Dates.compare_dates
 let sort : ((date * date) * 'a) array -> ((date * date) * 'a) array =
  fun arr ->
   let ret = Array.copy arr in
-  Array.sort (fun ((beg1, _), _) ((beg2, _), _) -> cmp beg1 beg2) ret;
+  Array.stable_sort (fun ((beg1, _), _) ((beg2, _), _) -> cmp beg1 beg2) ret;
   ret
 
 let one_month = Dates.make_period ~years:0 ~months:1 ~days:0
@@ -17,7 +17,7 @@ let split_by_month : date * date -> (date * date) array =
  fun (start, stop) ->
   let rec split start =
     let next = Dates.add_dates (Dates.first_day_of_month start) one_month in
-    if cmp stop next < 0 then [start, stop] else (start, next) :: split next
+    if cmp stop next <= 0 then [start, stop] else (start, next) :: split next
   in
   split start |> Array.of_list
 
@@ -37,7 +37,7 @@ let split_by_year : integer -> date * date -> (date * date) array =
     let next =
       Dates.add_dates (first_day_of_rolling_year start start_month) one_year
     in
-    if cmp stop next < 0 then [start, stop] else (start, next) :: split next
+    if cmp stop next <= 0 then [start, stop] else (start, next) :: split next
   in
   split start |> Array.of_list
 
