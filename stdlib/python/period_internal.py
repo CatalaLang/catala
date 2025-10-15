faef3be56fffea740b66aebde8b5b367dd034da2
Louis Gesbert <louis.gesbert@inria.fr>
2025-10-15 17:30:27 +0200

    Stdlib: implement Period externals in Python

--- stdlib/python/period_internal.py
+++ stdlib/python/period_internal.py
@@ -7,25 +7,35 @@
 from catala_runtime import *
 from typing import Any, List, Callable, Tuple
 from enum import Enum
+from dateutil import relativedelta
 
-
-def sort(p:List[Tuple[Date, Date]]):
-    pos = (SourcePosition(filename="stdlib/period_internal.catala_en",
-               start_line=6, start_column=13, end_line=6, end_column=17,
-               law_headings=[]))
-    raise Impossible(pos)
-    return sort__1
+def sort(p:List[Tuple[Tuple[Date, Date], Any]]):
+    return sorted(p, key=(lambda p: p[0][0]))
 
 def split_by_month(p:Tuple[Date, Date]):
-    pos = (SourcePosition(filename="stdlib/period_internal.catala_en",
-               start_line=9, start_column=13, end_line=9, end_column=27,
-               law_headings=[]))
-    raise Impossible(pos)
-    return split_by_month__1
+    cur = p[0]
+    end = p[1]
+    result = []
+    while cur < end:
+        cut = min(end, first_day_of_month(cur + Duration(relativedelta.relativedelta(months=1))))
+        result.append((cur, cut))
+        cur = cut
+    return result
+
+def first_day_of_next_rolling_year(start_month:int, date:Date):
+    assert (1 <= start_month and start_month <= 12)
+    if date.value.month < start_month:
+        yr = date.value.year
+    else:
+        yr = date.value.year + 1
+    return Date(datetime.date(year=yr, month=start_month, day=1))
 
 def split_by_year(start_month:Integer, p:Tuple[Date, Date]):
-    pos = (SourcePosition(filename="stdlib/period_internal.catala_en",
-               start_line=12, start_column=13, end_line=12, end_column=26,
-               law_headings=[]))
-    raise Impossible(pos)
-    return split_by_year__1
+    cur = p[0]
+    end = p[1]
+    result = []
+    while cur < end:
+        cut = min(end, first_day_of_next_rolling_year(start_month.value, cur))
+        result.append((cur, cut))
+        cur = cut
+    return result
