0c154805f18f579e13d6f4a01477e3fd7c3d7014
Louis Gesbert <louis.gesbert@inria.fr>
2025-10-15 17:30:27 +0200

    wip java periods

--- stdlib/java/period_internal.java
+++ stdlib/java/period_internal.java
@@ -1,11 +1,15 @@
-/* This is a template file following the expected interface and declarations to
- * implement the corresponding Catala module.
- *
- * You should replace all `Error.Impossible` place-holders with your
- * implementation and rename it to remove the ".template" suffix. */
-
 import catala.runtime.*;
 import catala.runtime.exception.*;
+
+class PeriodAssocComparator implements Comparator<CatalaTuple> {
+    @Override
+    public int compare(CatalaTuple assc1, CatalaTuple assc2) {
+        CatalaDate d1 = CatalaValue.<CatalaDate>cast(assc1.get(0));
+        CatalaDate d2 = CatalaValue.<CatalaDate>cast(assc1.get(1));
+        return d1.compareTo(d2);
+    }
+}
+
 
 public class Period_internal {
 
@@ -13,36 +17,31 @@ public class Period_internal {
 
         public static final CatalaFunction<CatalaArray<CatalaTuple>,CatalaArray<CatalaTuple>> sort =
             p -> {
+            CatalaArray<CatalaTuple> 
+            p.sort(new PeriodAssocComparator())
             final CatalaArray<CatalaTuple> sort__1;
             CatalaPosition pos =
                 new CatalaPosition
-                 ("stdlib/period_internal.catala_en", 6, 13, 6, 17,
-                  new String[]{});
+                 ("stdlib/period_internal.catala_en", 6, 13, 6, 17, new String[]{});
             throw new CatalaError(CatalaError.Error.Impossible, pos); };
-            // TODO
 
         public static final CatalaFunction<CatalaTuple,CatalaArray<CatalaTuple>> splitByMonth =
             p -> {
             final CatalaArray<CatalaTuple> splitByMonth__1;
             CatalaPosition pos =
                 new CatalaPosition
-                 ("stdlib/period_internal.catala_en", 9, 13, 9, 27,
-                  new String[]{});
+                 ("stdlib/period_internal.catala_en", 10, 13, 10, 27, new String[]{});
             throw new CatalaError(CatalaError.Error.Impossible, pos); };
-            // TODO
 
         public static final CatalaFunction<CatalaTuple,CatalaArray<CatalaTuple>> splitByYear =
             tup_arg_11 -> {
-            CatalaInteger startMonth = CatalaValue.<CatalaInteger>cast
-               (tup_arg_11.get(0));
+            CatalaInteger startMonth = CatalaValue.<CatalaInteger>cast(tup_arg_11.get(0));
             CatalaTuple p = CatalaValue.<CatalaTuple>cast(tup_arg_11.get(1));
             final CatalaArray<CatalaTuple> splitByYear__1;
             CatalaPosition pos =
                 new CatalaPosition
-                 ("stdlib/period_internal.catala_en", 12, 13, 12, 26,
-                  new String[]{});
+                 ("stdlib/period_internal.catala_en", 14, 13, 14, 26, new String[]{});
             throw new CatalaError(CatalaError.Error.Impossible, pos); };
-            // TODO
     }
 
 }
