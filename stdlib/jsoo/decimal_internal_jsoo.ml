e9771d6cf73b35d5d686eb537a891eca1d5c0670
Maxime Levillain <maxime.levillain@functori.com>
2026-02-02 13:10:48 +0100

    remove stdlib_internals from jsoo stdlib

--- stdlib/jsoo/decimal_internal_jsoo.ml
+++ stdlib/jsoo/decimal_internal_jsoo.ml
@@ -1,5 +1,5 @@
 open Catala_runtime_jsoo
-open Stdlib_internals.Decimal_internal
+open Decimal_internal
 
 let round_to_decimal_jsoo m n =
   decimal_to_jsoo @@ round_to_decimal (decimal_of_jsoo m) (integer_of_jsoo n)
