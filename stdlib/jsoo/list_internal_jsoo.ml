55fce353d5582782504fda504d12b4c2ca039077
Maxime Levillain <maxime.levillain@functori.com>
2026-02-05 13:43:31 +0100

    jsoo stdlib internal mli

--- stdlib/jsoo/list_internal_jsoo.ml
+++ stdlib/jsoo/list_internal_jsoo.ml
@@ -15,11 +15,32 @@ let remove_nth_element_jsoo arr n =
 
 let reverse_jsoo arr = Js.array (reverse (Js.to_array arr))
 
-let () =
-  Js.export "List_internal"
-    (object%js
-       method sequence start stop = sequence_jsoo start stop
-       method nth_element_ arr n = nth_element_jsoo arr n
-       method remove_nth_element_ arr n = remove_nth_element_jsoo arr n
-       method reverse arr = reverse_jsoo arr
-    end)
+class type default_ct = object
+  method sequence :
+    integer_jsoo -> integer_jsoo -> integer_jsoo Js.js_array Js.t Js.meth
+
+  method nth_element_ :
+    Js.Unsafe.any Js.js_array Js.t ->
+    integer_jsoo ->
+    Js.Unsafe.any Optional.jsoo Js.meth
+
+  method remove_nth_element_ :
+    Js.Unsafe.any Js.js_array Js.t ->
+    integer_jsoo ->
+    Js.Unsafe.any Js.js_array Js.t Js.meth
+
+  method reverse :
+    Js.Unsafe.any Js.js_array Js.t -> Js.Unsafe.any Js.js_array Js.t Js.meth
+end
+
+type default = default_ct Js.t
+
+let default : default =
+  object%js
+    method sequence start stop = sequence_jsoo start stop
+    method nth_element_ arr n = nth_element_jsoo arr n
+    method remove_nth_element_ arr n = remove_nth_element_jsoo arr n
+    method reverse arr = reverse_jsoo arr
+  end
+
+let () = Js.export "List_internal" default
--- stdlib/jsoo/money_internal_jsoo.ml
+++ stdlib/jsoo/money_internal_jsoo.ml
@@ -4,8 +4,16 @@ open Money_internal
 let round_to_decimal_jsoo m n =
   money_to_jsoo (round_to_decimal (money_of_jsoo m) (integer_of_jsoo n))
 
-let () =
-  Js_of_ocaml.Js.export "Money_internal"
-    (object%js
-       method round_to_decimal_ m n = round_to_decimal_jsoo m n
-    end)
+class type default_ct = object
+  method round_to_decimal_ :
+    money_jsoo -> integer_jsoo -> money_jsoo Js_of_ocaml.Js.meth
+end
+
+type default = default_ct Js_of_ocaml.Js.t
+
+let default : default =
+  object%js
+    method round_to_decimal_ m n = round_to_decimal_jsoo m n
+  end
+
+let () = Js_of_ocaml.Js.export "Money_internal" default
