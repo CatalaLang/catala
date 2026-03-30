75b84ee6560a0c2fa15aa26c25b353678a0d7384
Arnaud-Bihan <arnaud.bihan@functori.com>
2026-03-30 14:02:35 +0200

    Create the Python backend module
    Add build_system/backend/python.ml encapsulating Python-specific flags, Ninja rules, runtime statements, external copy, and catala invocation.

---
+++ build_system/backend/python.ml
@@ -0,0 +1,99 @@
+(* This file is part of the Catala build system, a specification language for
+   tax and social benefits computation rules. Copyright (C) 2020-2025 Inria,
+   contributors: Denis Merigoux <denis.merigoux@inria.fr>, Emile Rolley
+   <emile.rolley@tuta.io>, Louis Gesbert <louis.gesbert@inria.fr>
+
+   Licensed under the Apache License, Version 2.0 (the "License"); you may not
+   use this file except in compliance with the License. You may obtain a copy of
+   the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+   License for the specific language governing permissions and limitations under
+   the License. *)
+
+open Clerk_utils
+open Catala_utils
+
+module Flags = struct
+  let default
+      ~variables
+      ~autotest
+      ~use_default_flags
+      ~test_flags
+      ~include_dirs:_ =
+    let catala_flags_python =
+      (if autotest then ["--autotest"] else [])
+      @
+      if use_default_flags then ["-O"]
+      else
+        List.filter
+          (function
+            | "-O" | "--optimize" | "--closure-conversion" -> true | _ -> false)
+          test_flags
+    in
+    let def = Common.Flags.def ~variables in
+    [
+      def Var.catala_flags_python (lazy catala_flags_python);
+      def Var.python (lazy ["python3"]);
+    ]
+end
+
+module Backend = struct
+  open Var
+  open File
+  module Nj = Ninja_utils
+
+  let[@ocamlformat "disable"] static_base_rules =
+    [
+      Nj.rule "catala-python"
+        ~command:[!catala_exe; "python"; !catala_flags; !catala_flags_python;
+                  "-o"; !output; "--"; !input]
+        ~description:["<catala>"; "python"; "⇒"; !output];
+    ]
+
+  let external_copy item =
+    let catala_src = !Var.tdir / !Var.src in
+    let py, missing =
+      Ninja.extern_src ~filename:item.Scan.file_name ~backend:"python" ~ext:"py"
+        ~missing:[]
+    in
+    Ninja.check_missing ~backend:"python" ~module_def:item.Scan.module_def
+      ~missing ~filename:item.Scan.file_name;
+    List.to_seq
+      [
+        Nj.build "copy" ~implicit_in:[catala_src] ~inputs:[py]
+          ~outputs:[Ninja.target ~backend:"python" "py"];
+      ]
+
+  let runtime_build_statements ~stdbase =
+    let python_base = stdbase / "python" / "catala_runtime" in
+    let python_src = Var.(!runtime) / "python" / "src" / "catala" in
+    [
+      Nj.build "phony"
+        ~inputs:
+          [python_base -.- "py"; python_base /../ "dates.py"; Var.(!catala_exe)]
+        ~outputs:["@runtime-python"];
+      Nj.build "copy"
+        ~inputs:[python_src / "dates.py"]
+        ~outputs:[python_base /../ "dates.py"];
+      Nj.build "copy"
+        ~inputs:[python_src / "catala_runtime.py"]
+        ~outputs:[python_base -.- "py"];
+    ]
+
+  let catala ?vars ~inputs ~implicit_in _has_scope_tests =
+    Seq.return
+      (Nj.build "catala-python" ?vars ~inputs ~implicit_in
+         ~outputs:[Ninja.target ~backend:"python" "py"])
+
+  let build_object ~include_dirs:_ ~same_dir_modules:_ ~item:_ _has_scope_tests
+      =
+    []
+
+  let runtime_dir : File.t Lazy.t =
+    lazy File.(Lazy.force Poll.runtime_dir / "python" / "src" / "catala")
+end
