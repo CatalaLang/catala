8630e175fcb226ed342601dc1a697c2479ccda0c
Arnaud-Bihan <arnaud.bihan@functori.com>
2026-04-10 18:06:08 +0200

    static base rules takes backends

--- build_system/backend/java.ml
+++ build_system/backend/java.ml
@@ -167,7 +167,7 @@ module Backend = struct
       ]
   end
 
-  let[@ocamlformat "disable"] static_base_rules =
+  let[@ocamlformat "disable"] static_base_rules _ =
     [
       Nj.rule "catala-java"
         ~command:[!catala_exe; name; !catala_flags; !catala_flags_java;
