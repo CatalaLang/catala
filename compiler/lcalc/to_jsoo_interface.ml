652e0e8671d35039284adfad47c02efbeadc7906
Arnaud-Bihan <arnaud.bihan@functori.com>
2026-04-16 13:40:19 +0200

    Reexport used modules in jsoo interface

--- compiler/lcalc/to_jsoo_interface.ml
+++ compiler/lcalc/to_jsoo_interface.ml
@@ -684,6 +684,16 @@ let export_code_items ppml ppi modname exports ctx =
     modname;
   Format.fprintf ppi "val default : default@,"
 
+let reexport_used_modules ppml ppi used_modules =
+  List.iter
+    (fun (modulename, node) ->
+      if node.intf_id.is_external then ()
+      else
+        let info = ModuleName.get_info modulename in
+        pp [ppml; ppi] "module@ %a_jsoo@ =@ %a_jsoo@," Uid.MarkedString.format
+          info Uid.MarkedString.format info)
+    used_modules
+
 let format_program
     output_file
     ppml
@@ -711,6 +721,8 @@ let format_program
      @,"
     (fun fmt o -> Option.iter (Format.fprintf fmt "open %s@,") o)
     modname;
+  reexport_used_modules ppml ppi
+    (ModuleName.Map.bindings p.decl_ctx.ctx_modules);
   format_ctx ~include_:true type_ordering ppml ppi p.decl_ctx;
   let exports = format_code_items ppml ppi p.code_items in
   export_code_items ppml ppi modname exports p.decl_ctx;
