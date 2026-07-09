332e868ad15e68336912fa72f286bf5ce56a4006
PRIMET Romain <romain.primet@inria.fr>
2026-07-09 21:31:27 +0200

    clerk: unquote include dirs for direct-exec commands (run/typecheck/json-schema/exceptions)

--- build_system/clerk_driver.ml
+++ build_system/clerk_driver.ml
@@ -884,7 +884,9 @@ let run_targets
     if test_targets = [] then 0
     else
       let catala_flags =
-        let bdgs = Var.get_var var_bindings Var.catala_flags in
+        let bdgs =
+          List.map Var.unquote (Var.get_var var_bindings Var.catala_flags)
+        in
         if trace <> None then List.filter (( <> ) "--trace") bdgs else bdgs
       in
       let () =
@@ -1092,7 +1094,9 @@ let typecheck_cmd =
     with
     | exception Nothing_to_do -> Message.error "Nothing to typecheck."
     | target_items, var_bindings ->
-      let catala_flags = Var.get_var var_bindings Var.catala_flags in
+      let catala_flags =
+        List.map Var.unquote (Var.get_var var_bindings Var.catala_flags)
+      in
       let exec = Var.get_var var_bindings Var.catala_exe in
       let ret =
         List.filter_map
@@ -1569,7 +1573,9 @@ let json_schema_cmd =
         ~trace:false ~enabled_backends:[] ~config ~inplace:true
     in
     let catala_exe = Var.get_var var_bindings Var.catala_exe in
-    let catala_flags = Var.get_var var_bindings Var.catala_flags in
+    let catala_flags =
+      List.map Var.unquote (Var.get_var var_bindings Var.catala_flags)
+    in
     let cmd =
       catala_exe @ ["json-schema"; file; "--scope"; scope] @ catala_flags
     in
@@ -1597,7 +1603,9 @@ let exceptions_cmd =
         ~trace:false ~enabled_backends:[] ~config ~inplace:true
     in
     let catala_exe = Var.get_var var_bindings Var.catala_exe in
-    let catala_flags = Var.get_var var_bindings Var.catala_flags in
+    let catala_flags =
+      List.map Var.unquote (Var.get_var var_bindings Var.catala_flags)
+    in
     let cmd =
       catala_exe
       @ ["exceptions"; file; "--scope"; scope; "--variable"; variable]
