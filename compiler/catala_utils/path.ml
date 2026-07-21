1384f1478e812002e30137aa4b4aa20244eb124f
PRIMET Romain <romain.primet@inria.fr>
2026-07-21 18:07:24 +0200

    review: fix attributions, document backend_paths, tersen comments
    Per Denis's review: backend_paths.ml/.mli and path.ml/.mli are new files, so
    credit their actual author; document what pythonpath/classpath build.
    
    Tersen the quoting comments: var.ml comments duplicated the mli docs (kept as
    pointers); update the include_flags test comment, which still described the
    pre-emit-quoting rationale.

--- compiler/catala_utils/path.ml
+++ compiler/catala_utils/path.ml
@@ -1,6 +1,6 @@
 (* This file is part of the Catala compiler, a specification language for tax
-   and social benefits computation rules. Copyright (C) 2020 Inria, contributor:
-   Emile Rolley <emile.rolley@tuta.io>, Louis Gesbert <louis.gesbert@inria.fr>
+   and social benefits computation rules. Copyright (C) 2026 Inria,
+   contributors: Romain Primet <romain.primet@inria.fr>
 
    Licensed under the Apache License, Version 2.0 (the "License"); you may not
    use this file except in compliance with the License. You may obtain a copy of
