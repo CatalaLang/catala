72ef931aff1040e9e80f619277196009d23fbe60
Vincent Botbol <vincent.botbol@inria.fr>
2026-09-04 18:25:44 +0200

    Generate maven project files (pom.xml) for java backend

--- build_system/clerk_rules.mli
+++ build_system/clerk_rules.mli
@@ -40,6 +40,8 @@ type callback_info = {
   var_bindings : Var.bindings;
   modules_map : module_info String.Map.t;
   targets_map : Clerk_config.target String.Map.t;
+  target_deps : Clerk_config.target -> String.Set.t;
+      (** returns the names of the given target's dependencies *)
   linking_deps : Scan.item -> string list;
       (** item -> modules, topologically ordered *)
   inclusion_map : Scan.item String.Map.t;
