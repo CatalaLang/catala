b7413208b7c43d5a2d28f69d54cafc8614e1f0db
Louis Gesbert <louis.gesbert@inria.fr>
2026-09-08 16:40:01 +0200

    Clerk: generate simplistic dependency Makefiles in the C backend

--- build_system/clerk_backend/c.ml
+++ build_system/clerk_backend/c.ml
@@ -205,8 +205,25 @@ module Spec : Sig.Spec = struct
   let runtime_dir : File.t Lazy.t =
     lazy File.(Lazy.force Poll.runtime_dir / name)
 
-  let write_target_def_file ~config:_ ~dir:_ _target = ()
-  (* TODO: generate a Makefile ? Or at least a depfile ? *)
+  let write_target_def_file ~config:_ ~info ~dir target =
+    let open File in
+    with_out_channel (dir / "make.deps")
+    @@ fun oc ->
+    let modules =
+      String.Map.filter
+        (fun _ m ->
+          String.Set.mem target.Clerk_config.tname m.Module_graph.targets)
+        info.Module_graph.modules_map
+    in
+    String.Map.iter
+      (fun mname m ->
+        Printf.fprintf oc "%s.o: %s.c %s.h" (String.to_id mname)
+          (String.to_id mname) (String.to_id mname);
+        List.iter
+          (fun (dep, _) -> Printf.fprintf oc " %s.h" (String.to_id dep))
+          m.Module_graph.item.used_modules;
+        output_string oc "\n")
+      modules
 
   let install_runtime ~config =
     let open File in
--- build_system/clerk_backend/sig.ml
+++ build_system/clerk_backend/sig.ml
@@ -100,7 +100,11 @@ module type Spec = sig
       standard library files. Usually just the empty string. *)
 
   val write_target_def_file :
-    config:Clerk_cli.config -> dir:File.t -> Clerk_config.target -> unit
+    config:Clerk_cli.config ->
+    info:Module_graph.info ->
+    dir:File.t ->
+    Clerk_config.target ->
+    unit
   (** Called on the target directory under [_targets/<backend>/] after the files
       have been copied in *)
 
